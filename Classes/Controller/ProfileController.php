ec191a0e88b2d0da6ff84d269df5e510de1e00de
sascha_schieferdecker <sascha@sascha-schieferdecker.de>
2024-11-15 13:50:16 +0100

    Refactor ProfileController's feed fetching logic
    Refactor the feed fetching logic by extracting it into the new private method getFeedPosts. Simplify media URL updates with the new private method updateMediaUrls. Remove unused TokenRepository and adjust visibility of some class variables.

--- Classes/Controller/ProfileController.php
+++ Classes/Controller/ProfileController.php
@@ -5,7 +5,6 @@ namespace SaschaSchieferdecker\Instagram\Controller;
 
 use Psr\Http\Message\ResponseInterface;
 use SaschaSchieferdecker\Instagram\Domain\Repository\FeedRepository;
-use SaschaSchieferdecker\Instagram\Domain\Repository\TokenRepository;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
@@ -14,15 +13,18 @@ use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
  */
 class ProfileController extends ActionController
 {
+    private const CONTENT_TYPE_JSON = 'Content-Type: application/json';
 
     /**
      * @var string
      */
-    protected $imageFolder = 'typo3temp/assets/tx_instagram/';
+    private $imageFolder = 'typo3temp/assets/tx_instagram/';
+
     /**
      * @var FeedRepository
      */
-    protected $feedRepository = null;
+    private $feedRepository;
+
     /**
      * ProfileController constructor.
      * @param FeedRepository $feedRepository
@@ -34,44 +36,46 @@ class ProfileController extends ActionController
 
     public function showAction(): ResponseInterface
     {
-        if (strpos($this->settings['usernames'], "\n") !== false)
-        {
-            $usernames = explode("\n", $this->settings['usernames']);
-            $feedposts = $this->feedRepository->findDataByMultipleUsernames($usernames);
-        }
-        else
-        {
-            $feedposts = $this->feedRepository->findDataByUsername((string)$this->settings['usernames'], (int) $this->settings['limit']);
-        }
+        $feedPosts = $this->getFeedPosts();
         $this->view->assignMultiple([
-            'feedposts' => $feedposts,
+            'feedposts' => $feedPosts,
         ]);
         return $this->htmlResponse();
     }
 
-    public function jsonAction()
+    public function jsonAction(): void
     {
-        if (strpos($this->settings['usernames'], "\n") !== false)
-        {
-            $usernames = explode("\n", $this->settings['usernames']);
-            $feedposts = $this->feedRepository->findDataByMultipleUsernames($usernames);
+        $feedPosts = $this->getFeedPosts();
+        foreach ($feedPosts as &$item) {
+            $this->updateMediaUrls($item);
         }
-        else
-        {
-            $feedposts = $this->feedRepository->findDataByUsername((string)$this->settings['usernames'], (int) $this->settings['limit']);
-        }
-        foreach ($feedposts as &$item) {
-            $pathAndName = GeneralUtility::getFileAbsFileName($this->imageFolder) . $item['id'] . '.jpg';
-            if (file_exists($pathAndName)) {
-                $item['displayUrl'] = rtrim($this->settings['domainprefix'], '/') . '/' . $this->imageFolder . $item['id'] . '.jpg';
-            }
-            $pathAndName = GeneralUtility::getFileAbsFileName($this->imageFolder) . $item['id'] . '.mp4';
-            if (file_exists($pathAndName)) {
-                $item['videoUrl'] = rtrim($this->settings['domainprefix'], '/') . '/' . $this->imageFolder . $item['id'] . '.mp4';
-            }
-        }
-        header('Content-Type: application/json');
-        echo json_encode($feedposts);
+        header(self::CONTENT_TYPE_JSON);
+        echo json_encode($feedPosts);
         die();
+    }
+
+    private function getFeedPosts(): array
+    {
+        if (strpos($this->settings['usernames'], "\n") !== false) {
+            $usernames = explode("\n", $this->settings['usernames']);
+            return $this->feedRepository->findDataByMultipleUsernames($usernames);
+        }
+        return $this->feedRepository->findDataByUsername((string)$this->settings['usernames'], (int)$this->settings['limit']);
+    }
+
+    private function updateMediaUrls(array &$item): void
+    {
+        $absPath = GeneralUtility::getFileAbsFileName($this->imageFolder);
+        $domainPrefix = rtrim($this->settings['domainprefix'], '/');
+
+        $imagePath = $absPath . $item['id'] . '.jpg';
+        if (file_exists($imagePath)) {
+            $item['displayUrl'] = $domainPrefix . '/' . $this->imageFolder . $item['id'] . '.jpg';
+        }
+
+        $videoPath = $absPath . $item['id'] . '.mp4';
+        if (file_exists($videoPath)) {
+            $item['videoUrl'] = $domainPrefix . '/' . $this->imageFolder . $item['id'] . '.mp4';
+        }
     }
 }
